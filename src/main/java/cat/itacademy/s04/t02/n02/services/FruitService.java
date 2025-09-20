cae442c91704840d97d13a53d85ffea009c6140c
resikrys <resicris@gmail.com>
2025-09-20 13:42:31 +0200

    add FruitService class & crud logic

---
+++ src/main/java/cat/itacademy/s04/t02/n02/services/FruitService.java
@@ -0,0 +1,44 @@
+package cat.itacademy.s04.t02.n02.services;
+
+import cat.itacademy.s04.t02.n02.exceptions.FruitNotFoundException;
+import cat.itacademy.s04.t02.n02.models.Fruit;
+import cat.itacademy.s04.t02.n02.models.FruitDTO;
+import cat.itacademy.s04.t02.n02.repository.FruitRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FruitService {
+
+    @Autowired
+    private FruitRepository fruitRepository;
+
+    public Fruit createFruit(FruitDTO fruitDTO) {
+        Fruit fruit = new Fruit(fruitDTO.getName(), fruitDTO.getQuantityKilos());
+        return fruitRepository.save(fruit);
+    }
+
+    public Fruit getOneFruit(int id) {
+        return fruitRepository.findById(id)
+                .orElseThrow(() -> new FruitNotFoundException("Fruit with id " + id + " not found."));
+    }
+
+    public List<Fruit> getAllFruits() {
+        return fruitRepository.findAll();
+    }
+
+    public Fruit updateFruit(int id, FruitDTO fruitDTO) {
+        Fruit existingFruit = getOneFruit(id);
+        existingFruit.setName(fruitDTO.getName());
+        existingFruit.setQuantityKilos(fruitDTO.getQuantityKilos());
+        return fruitRepository.save(existingFruit);
+    }
+
+    public void deleteFruit(int id) {
+        Fruit fruit = getOneFruit(id);
+        fruitRepository.delete(fruit);
+    }
+
+}
